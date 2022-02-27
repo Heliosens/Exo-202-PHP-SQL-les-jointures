f86c6825a55c0c8675c99b9886456d5be7cc2b22
Sylvie-B <unsolo@hotmail.fr>
2022-02-27 18:57:52 +0100

    inner join

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
  * 2. Connectez vous à la base de données blog.
  */
 
+require 'connPDO.php';
+$pdo = new connPDO();
+$db = $pdo->conn();
+
 /**
  * 3. Sans utiliser les alias, effectuez une jointure de type INNER JOIN de manière à récupérer :
  *   - Les articles :
@@ -17,7 +21,17 @@
  */
 
 // TODO Votre code ici.
+$request = $db->prepare("
+    SELECT article.id, article.title, article.content , categorie.name
+    FROM article
+    INNER JOIN categorie ON article.category_fk = categorie.id
+");
 
+$request->execute();
+;
+foreach ($request->fetchAll() as $item){
+    echo $item['id'] . " | " . $item['title'] . " | " . $item['content'] . " | " . $item['name'] . "<br>";
+}
 
 /**
  * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
